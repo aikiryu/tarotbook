67282e02baf885120adc2f2e3e9a8f62c6548712
Aiki <aikiryu1228@naver.com>
2023-10-01 14:02:12 +0900

    yaho

--- js/main.js
+++ js/main.js
@@ -1,46 +1,53 @@
-function animateFrom(elem, direction) {
-    direction = direction || 1;
-    var x = 0,
-        y = direction * 100;
-    if(elem.classList.contains("gs_reveal_fromTop")) {
-      x = 0;
-      y = 60;
-    } else if (elem.classList.contains("gs_reveal_fromRight")) {
-      x = 100;
-      y = 0;
-    }
-    elem.style.transform = "translate(" + x + "px, " + y + "px)";
-    elem.style.opacity = "0";
-    gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
-      duration: 1.25, 
-      x: 0,
-      y: 0, 
-      autoAlpha: 1, 
-      ease: "expo", 
-      overwrite: "auto"
-    });
+gsap.registerPlugin(ScrollTrigger);
+
+let items = gsap.utils.toArray(".items");
+let txtBox = document.querySelector('.pic-container');
+
+
+// pin box-c when it reaches the center of the viewport, for 300px
+
+
+let largeTL = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".items.item1",
+    pin: ".s2 .txtBox",
+    scrub: 0.5,
+    start: "top 100",
+    //end: '+=550%',
+    end: "+=1600"
   }
+
+})
+
+
+items.forEach(function (elem, i) {
+  gsap.set(".s2 .items", { zIndex: (i, target, targets) => targets.length - i });
+
+  // select the relevant elements
+  //var lines = elem.querySelectorAll(".lines");
+
+  const tlDelay = i;
+
+  var smallTL = gsap.timeline();
+
+  smallTL
+    .to(elem, { 
+      autoAlpha: 1,
+      duration: 1, }, tlDelay)
+    // .from(lines, {
+    //   xPercent: -100,
+    //   duration: 1,
+    //   ease: "power2.out",
+    //   stagger: 0.6
+    // })
+    .to(elem, { 
+      autoAlpha: 0,})
+    .to({}, {duration:1,autoAlpha: 1,}) // a little pause at the end
+  largeTL.add(smallTL, tlDelay);
+
   
-  function hide(elem) {
-    gsap.set(elem, {autoAlpha: 0});
-  }
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    gsap.registerPlugin(ScrollTrigger);
-    
-    gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
-      hide(elem); // assure that the element is hidden when scrolled into view
-      
-      ScrollTrigger.create({
-        trigger: elem,
-        markers: false,
-        start:'top 500',
-        onEnter: function() { animateFrom(elem) }, 
-        onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
-      });
-    });
-  });
-  
+});
+
 
 
   $(window).resize(function(){
